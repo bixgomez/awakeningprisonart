4c701c5700d6a1c747952721191040af725e9c1e
Richard Gilbert <gilbert67@gmail.com>
2023-02-21 07:59:19 -0800

    Further updates.

--- app/public/wp-content/themes/prisonart_tw/theme/template-parts/content/teaser--artwork.php
+++ app/public/wp-content/themes/prisonart_tw/theme/template-parts/content/teaser--artwork.php
@@ -1,14 +1,20 @@
 <?php
 
-echo '<div class="teaser pb-6 mb-6 border-b border-[#006667]">';
+echo '<div class="teaser pt-6 mb-6 border-t border-[#006667]">';
 
   $title = isset($args['title']) ? $args['title'] : '';
   $dimensions = isset($args['dimensions']) ? $args['dimensions'] : '';
   $description = isset($args['description']) ? $args['description'] : '';
   $image_id = isset($args['image_id']) ? $args['image_id'] : '';
+  $parsed = parse_url( wp_get_attachment_url( $image_id ) );
+  $imgurl = dirname( $parsed [ 'path' ] ) . '/' . rawurlencode( basename( $parsed[ 'path' ] ) );
 
   echo '<div class="teaser-image">';
+
+    echo '<a target="_blank" href="'.$imgurl.'">';
     echo wp_get_attachment_image( $image_id, 'medium_large' );
+    echo '</a>';
+
   echo '</div>';
 
   echo '<div class="teaser-text ">';
@@ -16,8 +22,13 @@ echo '<div class="teaser pb-6 mb-6 border-b border-[#006667]">';
     echo '<p class="teaser-dimensions text-foreground">' . $dimensions . '</p>';
     echo '<p class="teaser-description text-foreground">' . $description . '</p>';
     ?>
-    To purchase this piece, please send an email to deansluyter@gmail.com, specifying the name of the piece ("<?php echo $title; ?>"),
-    the price you would like to pay for it, your mailing address, and any additional comments or questions.  You will be billed via PayPal.
+    <div class="purchase-info">
+      To purchase this piece, please send an email to 
+      <a href="mailto:deansluyter@gmail.com">deansluyter@gmail.com</a>, 
+      specifying the name of the piece <i>("<?php echo $title; ?>")</i>,
+      the price you would like to pay for it, your mailing address, and 
+      any additional comments or questions.  You will be billed via PayPal.
+    </div>
     <?php
   echo '</div>';
 
